d5445df1f75e5daec494f063a46eba40ec5a33d2
Tim Sarvey <timsarvey@gmail.com>
2015-10-22 10:23:43 -0400

    Add pipe entitiy and graphics component.

--- flappy-bird-game/js/flappy_bird.js
+++ flappy-bird-game/js/flappy_bird.js
@@ -1,8 +1,10 @@
 var graphicsSystem = require('./systems/graphics');
 var bird = require('./entities/bird');
+var pipe = require('./entities/pipe');
 
 var FlappyBird = function() {
     this.entities = [new bird.Bird()];
+    this.entities.push(new pipe.Pipe());
     this.graphics = new graphicsSystem.GraphicsSystem(this.entities);
 };
 
